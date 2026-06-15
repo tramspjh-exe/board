eb131d8265febc78494759b51dc4a434d6a332d4
jihwan <tramspjh@gmail.com>
2026-06-15 22:00:59 +0900

    Update write.php

--- write.php
+++ write.php
@@ -1,3 +1,11 @@
+<?php
+session_start();
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+    exit;
+}
+$board_type = isset($_GET['board']) ? $_GET['board'] : 'free';
+?>
 <!DOCTYPE html>
 <html lang="ko">
 <head>
@@ -5,13 +13,24 @@
     <title>글쓰기</title>
 </head>
 <body>
-    <h1>새 글 작성</h1>
+    <h1>새 글 작성 (<?= ($board_type === 'free') ? '자유게시판' : 'Q&A게시판' ?>)</h1>
     <form action="write_process.php" method="POST" enctype="multipart/form-data">
-        <p>제목: <input type="text" name="title" required></p>
-        <p>본문: <textarea name="content" rows="10" cols="50" required></textarea></p>
-        <p>파일 첨부: <input type="file" name="attachment"></p>
+        <input type="hidden" name="board_type" value="<?= htmlspecialchars($board_type) ?>">
+        <div>
+            제목: <input type="text" name="title" required style="width: 500px;">
+        </div>
+        <br>
+        <div>
+            내용:<br>
+            <textarea name="content" rows="10" cols="70" required></textarea>
+        </div>
+        <br>
+        <div>
+            파일 첨부: <input type="file" name="attachment">
+        </div>
+        <br>
         <button type="submit">작성 완료</button>
-        <a href="index.php">취소</a>
+        <a href="index.php?board=<?= $board_type ?>"><button type="button">취소</button></a>
     </form>
 </body>
 </html>
